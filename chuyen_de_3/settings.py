47f6c26890853288ad62f9dd7a4268628cef5ef9
Mai Quoc Viet <duvimuahe123@gmail.com>
2024-10-31 08:13:20 +0700

    update views and setting

--- chuyen_de_3/settings.py
+++ chuyen_de_3/settings.py
@@ -124,3 +124,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
+# Cài đặt session engine nếu cần
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
--- myapp/views.py
+++ myapp/views.py
@@ -3,14 +3,21 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def get_resource(request):
-    id_value = request.GET.get('id')
-    user_ip = request.META.get('REMOTE_ADDR')  # Lấy địa chỉ IP của người dùng
+    # Lấy địa chỉ IP từ request
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        user_ip = x_forwarded_for.split(',')[0]  # Lấy IP đầu tiên
+    else:
+        user_ip = request.META.get('REMOTE_ADDR')  # Nếu không có, lấy REMOTE_ADDR
+
+    id_value = request.GET.get('id')  # Lấy giá trị ID từ query string
 
     # Truyền cả IP và ID vào context để hiển thị trên trang
     return render(request, 'myapp/index.html', {
         'message': f"Đang truy cập tài nguyên có ID = {id_value}",
         'user_ip': user_ip
     })
+
 
 
 def index(request):
